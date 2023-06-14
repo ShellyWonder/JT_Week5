be278658a6fe3bb8b85040b49b306cf9e1b4dab8
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-06-14 11:12:46 -0500

    exercise 3 functionality
    added

--- src/com/wonderwebdev/UserApp.java
+++ src/com/wonderwebdev/UserApp.java
@@ -5,7 +5,10 @@ public class UserApp {
 	public static void main(String[] args) {
 		UserService userService = new UserService();
 		User user = userService.createUser("JDoe@mailinator.com","Abc&123!", "Joe");
+		String[]parsedValues = userService.parseText("JaneD@mailinator.com,123&Abc?,Jane");
 		System.out.println(user);
+		System.out.println(parsedValues);
+		
 	}
 
 }
--- src/com/wonderwebdev/UserService.java
+++ src/com/wonderwebdev/UserService.java
@@ -1,15 +1,21 @@
 package com.wonderwebdev;
 
-
 public class UserService {
 
 	public User createUser(String username, String password, String name) {
-	 User user = new User(username, password, name);
-	 user.setUsername(username);
-	 user.setPassword(password);
-	 user.setName(name);
-	 
-	 return user;
-	 
+		User user = new User(username, password, name);
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setName(name);
+
+		return user;
+
 	}
+
+	public String[] parseText(String input) {
+		// return an array of Strings with three elements inside
+           String[] parsedValues = input.split(",");
+          return parsedValues;
+		
+          }
 }
